15c3354ffe49a0fdd2363c0340de6523dd78aaea
Pratik <ghugepratik52@gmail.com>
2025-09-04 00:05:22 +0530

    Resolved naming confliction

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -33,6 +33,6 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('authentication.urls')),
+    path('api/', include('Authentication_app.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
